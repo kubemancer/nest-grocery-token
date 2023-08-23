c7e5b1bcb95d302abe48986a549da1f11a6b9963
Khalil <khalil.chettaoui06@gmail.com>
2023-08-23 22:59:31 +0100

    🐳 dockerfile changes

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(TokenModule, {
     transport: Transport.TCP,
     options: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: new ConfigService().get('port'),
     },
   } as TcpOptions);
